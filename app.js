687df3b40948dce2957435f61402b5def7a5cfe6
Aastha Singhal <aastha.tanya21@gmail.com>
2023-06-14 22:23:59 +0530

    email changes

--- app.js
+++ app.js
@@ -16,3 +16,26 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+function sendMail(){
+  var params = {
+    name: document.getElementById("username").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value, 
+  };
+
+  const serviceId=  "service_w2nuba6";
+  const templateId= "template_5z88guk";
+
+  emailjs.send(servideId, templateId, params)
+  .then(
+    res=>{
+      document.getElementById("username").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value= "";
+      console.log(res);
+      alert("your message sent successfully");
+    }
+  )
+  .catch((err) => console.log(err));
+}
